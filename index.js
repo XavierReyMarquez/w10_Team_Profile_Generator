6195e3fa3a27e57666b729c1fdf84d0713b65f66
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-04-26 19:35:42 -0700

    inital file setup node, inquirer, and npm istall

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./manager");
+
+// create manager function
+const manager = () => {
+  // put inquirer prompt to get info
+  // in the .then get results
+  // used manger class and result to create new varaible
+  // push to globe array
+};
+manager();
---
+++ index.test.js
@@ -0,0 +1,4 @@
+test("pratice", function () {
+  var num = 2;
+  expect(num).toBe(2);
+});
