cec658d241989d3dbb8c07b464404da1b7fcae26
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-04-29 00:22:44 -0700

    adding prompt question to manager and created/finished engineer.js and inter.js

---
+++ Inter.js
@@ -0,0 +1,17 @@
+const Employee = require("./Employee");
+const Engineer = require("./Engineer");
+
+class Inter extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+  getSchool() {
+    return this.school;
+  }
+  getRole() {
+    return "Inter";
+  }
+}
+
+module.exports = Engineer;
--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Manager = require("./manager");
+const Engineer = require("./Employee");
 
 // create manager function
-const manager = () => {
+const PromptManager = () => {
   // put inquirer prompt to get info
-  // in the .then get results
-  // used manger class and result to create new varaible
-  // push to globe array
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "what is the managers name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "what is the managers ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "what is the managers email?",
+        },
+        {
+          type: "input",
+          name: "officeNum",
+          message: "what is the managers office number?",
+        },
+      ])
+      // in the .then get results
+      .then((managerdata) => {
+        console.log(managerdata);
+        const { name, id, email, officeNum } = managerdata;
+        // used manger class and result to create new varaible
+        employee = new Manager(name, id, email, officeNum);
+        let role = { role: "Manager" };
+        // push to globe array
+        return { ...managerdata, ...role };
+      })
+  );
 };
-manager();
+
+const promptEmployee = (managerdata) => {};
+
+PromptManager();
